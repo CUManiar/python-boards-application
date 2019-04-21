96a2ae3ccc42e56f00679f8ff5fca73964b359f7
chiragma <maniarchirag57@gmail.com>
2019-04-22 00:52:21 +0530

    - New topic added breadcrumb - Test cases for new topic
    Signed-off-by: chiragma <maniarchirag57@gmail.com>

--- boards/urls.py
+++ boards/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     # path('', views.home, name='home'),
     path('', views.home, name='home'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
 ]
